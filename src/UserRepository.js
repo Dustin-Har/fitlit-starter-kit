480057b624750af691e75ee16ab30e4e0be36fe0
Ryan Miller <ryandmiller3521@gmail.com>
2021-03-28 20:11:11 -0600

    Add method to return a user with specified userId

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -2,6 +2,11 @@ class UserRepository {
     constructor(data) {
         this.data = data;
     }
+
+    returnUser(userId) {
+        const user = this.data.find(user => user.id === userId);
+        return user;
+    }
 }
 
 module.exports = UserRepository;
