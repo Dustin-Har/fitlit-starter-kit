51e47affc7356f81cebf692c44c139a286cbe718
Ryan Miller <ryandmiller3521@gmail.com>
2021-03-28 20:34:54 -0600

    Add method to find average step goal amoung all users

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -7,6 +7,13 @@ class UserRepository {
         const user = this.data.find(user => user.id === userId);
         return user;
     }
+
+    averageStepGoal() {
+        let sum = this.data.reduce((acc, user) => {
+            return acc + user.dailyStepGoal;
+        }, 0);
+        return sum / this.data.length;
+    }
 }
 
 module.exports = UserRepository;
