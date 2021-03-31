8183d3544808ad6eb79c11e68496a0339ee80aec
Ryan Miller <ryandmiller3521@gmail.com>
2021-03-31 15:53:41 -0600

    Add hydration graphs

--- src/scripts.js
+++ src/scripts.js
@@ -3,14 +3,85 @@ const userName = document.getElementById('userName');
 const userAddress = document.getElementById('userAddress');
 const userEmail = document.getElementById('userEmail');
 const userStride = document.getElementById('userStride');
+const waterDailyBar = document.getElementById('waterBarDaily').getContext('2d');
+const weeklyWaterBar = document.getElementById('waterBarWeekly').getContext('2d');
+
 
 let userRepository = new UserRepository(userData);
 let user = new User(userRepository.returnUser(4));
+let hydration = new Hydration(hydrationData);
+let waterDailyChart = new Chart(waterDailyBar, {
+    type: 'bar',
+
+    data: {
+        datasets: [{
+            label: 'Daily Water Consumption',
+            backgroundColor: 'lightblue',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [hydration.waterDayConsumed(user.id, '2019/09/22'), 0 , 130]
+        }]
+    },
+    options: {
+        legend: {
+            display: false
+        }, 
+
+        scales: {
+            yAxes: [{ticks:{display:false}}]
+        }
+    }
+});
+let waterWeeklyChart = new Chart(weeklyWaterBar, {
+    type: 'bar',
+
+    data: {
+        labels: ['Day1', 'Day2', 'Day3', 'Day4', 'Day5', 'Day6', 'Day7'],
+        datasets: [{
+            label: 'Weekkly Water Consumption in Ounces',
+            backgroundColor: 'lightblue',
+            borderColor: 'rgb(255, 99, 132)',
+            data: hydration.weekConsumption(user, '2019/09/22')
+        }]
+    },
+
+    options: {}
+});
+// let waterWeeklyChart = new Chart(weeklyWaterBar, {
+//     type: 'bar',
+
+//     labels: ['Day1', 'Day2', 'Day3', 'Day4', 'Day5', 'Day6', 'Day7'],
+
+//     data: {
+//         datasets: [{
+//             label: 'Weekly Water Consumption',
+//             backgroundColor: 'lightblue',
+//             borderColor: 'rgb(255, 99, 132)',
+//             data: [37,54,22,10,60,100,76]
+//         }]
+//     },
+
+// });
+
+
+
 changeUserInformation();
-displayStep();
+displayUserInformation();
+
+
+
+
+function displayUserInformation() {
+    displayStep();
+    displayHydration();
+}
 
 function displayStep() {
     console.log(`${userRepository.averageStepGoal()} average step goal amongst user Your Goal ${user.dailyStepGoal}`);
+}
+
+function displayHydration() {
+    waterDailyChart.data.datasets[0].data[0] = hydration.waterDayConsumed(user.id, '2019/09/22');
+    waterDailyChart.update();
 }
 
 function changeUserInformation() {
