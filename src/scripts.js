d46148672817fa5824db5f811ca4f78905a83d8d
Ryan Miller <ryandmiller3521@gmail.com>
2021-03-31 15:56:10 -0600

    Add variable to todays date which is the latest day in the data we have

--- src/scripts.js
+++ src/scripts.js
@@ -6,7 +6,7 @@ const userStride = document.getElementById('userStride');
 const waterDailyBar = document.getElementById('waterBarDaily').getContext('2d');
 const weeklyWaterBar = document.getElementById('waterBarWeekly').getContext('2d');
 
-
+let todaysDate = '2019/09/22';
 let userRepository = new UserRepository(userData);
 let user = new User(userRepository.returnUser(4));
 let hydration = new Hydration(hydrationData);
@@ -18,7 +18,7 @@ let waterDailyChart = new Chart(waterDailyBar, {
             label: 'Daily Water Consumption',
             backgroundColor: 'lightblue',
             borderColor: 'rgb(255, 99, 132)',
-            data: [hydration.waterDayConsumed(user.id, '2019/09/22'), 0 , 130]
+            data: [hydration.waterDayConsumed(user.id, todaysDate), 0 , 130]
         }]
     },
     options: {
@@ -40,35 +40,15 @@ let waterWeeklyChart = new Chart(weeklyWaterBar, {
             label: 'Weekkly Water Consumption in Ounces',
             backgroundColor: 'lightblue',
             borderColor: 'rgb(255, 99, 132)',
-            data: hydration.weekConsumption(user, '2019/09/22')
+            data: hydration.weekConsumption(user, todaysDate)
         }]
     },
 
     options: {}
 });
-// let waterWeeklyChart = new Chart(weeklyWaterBar, {
-//     type: 'bar',
-
-//     labels: ['Day1', 'Day2', 'Day3', 'Day4', 'Day5', 'Day6', 'Day7'],
-
-//     data: {
-//         datasets: [{
-//             label: 'Weekly Water Consumption',
-//             backgroundColor: 'lightblue',
-//             borderColor: 'rgb(255, 99, 132)',
-//             data: [37,54,22,10,60,100,76]
-//         }]
-//     },
-
-// });
-
-
 
 changeUserInformation();
 displayUserInformation();
-
-
-
 
 function displayUserInformation() {
     displayStep();
@@ -80,8 +60,10 @@ function displayStep() {
 }
 
 function displayHydration() {
-    waterDailyChart.data.datasets[0].data[0] = hydration.waterDayConsumed(user.id, '2019/09/22');
+    waterDailyChart.data.datasets[0].data[0] = hydration.waterDayConsumed(user.id, todaysDate);
     waterDailyChart.update();
+    waterWeeklyChart.data.datasets[0].data = hydration.weekConsumption(user, todaysDate);
+    waterWeeklyChart.update();
 }
 
 function changeUserInformation() {
