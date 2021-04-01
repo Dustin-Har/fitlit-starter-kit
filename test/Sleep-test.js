db528b1023bf651dd0a86905543137867c043a59
Ryan Miller <ryandmiller3521@gmail.com>
2021-04-01 13:39:15 -0600

    Add sleep as a function and add data for testing purpose

---
+++ test/Sleep-test.js
@@ -0,0 +1,114 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const User = require('../src/User');
+const Sleep = require('../src/Sleep');
+
+describe('Sleep', function() {
+    let user, sleep;
+    const sleepData = [
+        {
+          "userID": 1,
+          "date": "2019/06/15",
+          "hoursSlept": 6.1,
+          "sleepQuality": 2.2
+        },
+        {
+          "userID": 2,
+          "date": "2019/06/08",
+          "hoursSlept": 7,
+          "sleepQuality": 4.7
+        },
+        {
+          "userID": 3,
+          "date": "2019/06/15",
+          "hoursSlept": 10.8,
+          "sleepQuality": 4.7
+        },
+        {
+          "userID": 4,
+          "date": "2019/06/15",
+          "hoursSlept": 5.4,
+          "sleepQuality": 3
+        },
+        {
+            "userID": 1,
+            "date": "2019/06/16",
+            "hoursSlept": 4.1,
+            "sleepQuality": 3.8
+          },
+          {
+            "userID": 2,
+            "date": "2019/06/09",
+            "hoursSlept": 7.5,
+            "sleepQuality": 3.8
+          },
+          {
+            "userID": 3,
+            "date": "2019/06/16",
+            "hoursSlept": 10.7,
+            "sleepQuality": 3.4
+          },
+          {
+            "userID": 4,
+            "date": "2019/06/16",
+            "hoursSlept": 8.3,
+            "sleepQuality": 4.5
+          },
+          {
+            "userID": 2,
+            "date": "2019/06/10",
+            "hoursSlept": 7.3,
+            "sleepQuality": 4.3
+          },
+          {
+            "userID": 2,
+            "date": "2019/06/11",
+            "hoursSlept": 9.9,
+            "sleepQuality": 3
+          },
+          {
+            "userID": 2,
+            "date": "2019/06/12",
+            "hoursSlept": 10.6,
+            "sleepQuality": 1.4
+          },
+          {
+            "userID": 2,
+            "date": "2019/06/13",
+            "hoursSlept": 4.7,
+            "sleepQuality": 4.5
+          },
+          {
+            "userID": 2,
+            "date": "2019/06/14",
+            "hoursSlept": 4.5,
+            "sleepQuality": 1.2
+          },
+          {
+            "userID": 2,
+            "date": "2019/06/15",
+            "hoursSlept": 4.6,
+            "sleepQuality": 1.7
+          }];
+    const userData = {
+        "id": 2,
+        "name": "Luisa Hane",
+        "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+        "email": "Diana.Hayes1@hotmail.com",
+        "strideLength": 4.3,
+        "dailyStepGoal": 10000,
+        "friends": [
+        16,
+        4,
+        8
+        ]
+    }
+    beforeEach(function() {
+        user = new User(userData);
+        sleep = new Sleep(sleep);
+    });
+    it('Should be a function', function() {
+        expect(Sleep).to.be.a('function');
+    });
+});
